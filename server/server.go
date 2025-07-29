3b30f9cd31b10ff20ea131d1b62b26357bef1c0e
Danik <84225625+DanCpp@users.noreply.github.com>
2025-07-29 11:29:21 +0800

    Add some new methods to structure and end the server

--- server/server.go
+++ server/server.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"net"
+
+	"github.com/DanCpp/GoNetwork/sserver"
+)
+
 func main() {
+	srv, err := net.Listen("tcp4", ":80")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	server := sserver.NewServer(srv)
+	defer server.DeleteServer()
+
+	for {
+		_, err := server.AcceptNewClient()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+	}
 }
--- server/sserver/sserver.go
+++ server/sserver/sserver.go
@@ -1,6 +1,7 @@
 package sserver
 
 import (
+	"fmt"
 	"net"
 )
 
@@ -22,4 +23,34 @@ func (srv *server) AddConnection(new_connection net.Conn) {
 
 func (srv *server) DeleteServer() {
 	srv.serv_listener.Close()
+}
+
+func (srv *server) AcceptNewClient() (net.Conn, error) {
+	connection, err := srv.serv_listener.Accept()
+	if err != nil {
+		return nil, err
+	}
+	srv.connections[connection] = 1
+	go srv.handleConnection(connection)
+	return connection, nil
+}
+
+func (srv *server) handleConnection(connection net.Conn) {
+	defer connection.Close()
+	defer delete(srv.connections, connection)
+	for {
+		input_buffer := make([]byte, 1024)
+		n, err := connection.Read(input_buffer)
+		if n == 0 || err != nil {
+			fmt.Println(err)
+			break
+		}
+		input_string := string(input_buffer[0:n])
+		for key, value := range srv.connections {
+			if key == connection || value == 0 {
+				continue
+			}
+			key.Write([]byte(input_string))
+		}
+	}
 }
